48e30397db6dd97feefc07c98353ff3524ac4c56
ƒnaxi <fnanex.74@gmail.com>
2026-07-22 09:39:53 +0300

    Refactor temporary voices service

--- Gir/Services/Client/TemporaryVoicesService.cs
+++ Gir/Services/Client/TemporaryVoicesService.cs
@@ -16,7 +16,7 @@ public class CTemporaryVoicesService : CServiceBase
 		Client.VoiceStateUpdated += Client_OnVoiceStateUpdated;
 	}
 	
-	private const string TempVoiceName = "Room";
+	private const string RoomName = "Room";
 	
 	private async Task Client_OnGuildDownloadCompleted(DiscordClient Sender, GuildDownloadCompletedEventArgs Args)
 	{
@@ -33,9 +33,7 @@ public class CTemporaryVoicesService : CServiceBase
 					DiscordChannel CreateVoiceChannel = Guild.GetChannel(CChannel.CreateVoice);
 					foreach (DiscordMember? Member in Channel.Users)
 					{
-						// TODO: This code repeats Client_OnVoiceStateUpdated
-						string Name = $"🜋 {Member.DisplayName}'s {TempVoiceName}";
-						DiscordChannel NewChannel = await Guild.CreateVoiceChannelAsync(Name, Guild.GetChannel(CCategory.VoiceChannels), null, CreateVoiceChannel.UserLimit);
+						DiscordChannel NewChannel = await CreateTempVoice(Guild, Member.DisplayName);
 
 						await Member.ModifyAsync(x => x.VoiceChannel = NewChannel);
 					}
@@ -53,14 +51,11 @@ public class CTemporaryVoicesService : CServiceBase
 	{
 		DiscordGuild Guild = Args.Guild;
 		
-		DiscordChannel CreateVoiceChannel = Guild.GetChannel(CChannel.CreateVoice);
 		if (Args.After?.Channel != null && Args.After.Channel.Id == CChannel.CreateVoice)
 		{
-			DiscordMember Member = await Guild.GetMemberAsync(Args.User.Id);
-
-			string Name = $"🜋 {Member.DisplayName}'s {TempVoiceName}";
-			DiscordChannel NewChannel = await Guild.CreateVoiceChannelAsync(Name, Guild.GetChannel(CCategory.VoiceChannels), null, CreateVoiceChannel.UserLimit);
-
+			DiscordMember  Member     = await Guild.GetMemberAsync(Args.User.Id);
+			DiscordChannel NewChannel = await CreateTempVoice(Guild, Member.DisplayName);
+			
 			await Member.ModifyAsync(x => x.VoiceChannel = NewChannel);
 		}
 		else if (Args.Before?.Channel != null && IsTempVoice(Args.Before.Channel))
@@ -71,6 +66,19 @@ public class CTemporaryVoicesService : CServiceBase
 	
 	private static bool IsTempVoice(DiscordChannel Channel)
 	{
-		return Channel.Id is not (CChannel.CreateVoice or CChannel.AFK) && Channel.Name.Contains(TempVoiceName);
+		return Channel.Id is not (CChannel.CreateVoice or CChannel.AFK) && Channel.Name.Contains(RoomName);
+	}
+	
+	private async Task<DiscordChannel> CreateTempVoice(DiscordGuild Guild, string MemberName)
+	{
+		string         Name               = GenerateTempVoiceChannelName(MemberName);
+		DiscordChannel CreateVoiceChannel = Guild.GetChannel(CChannel.CreateVoice);
+
+		return await Guild.CreateVoiceChannelAsync(Name, Guild.GetChannel(CCategory.VoiceChannels), null, CreateVoiceChannel.UserLimit);
+	}
+	
+	private string GenerateTempVoiceChannelName(string MemberName)
+	{
+		return $"🜋 {MemberName}'s {RoomName}";
 	}
 }
